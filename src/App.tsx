3102ddac2237d1165adf4e660f257068d22257fb
William Chan <chan.william.ts@gmail.com>
2024-03-01 17:38:19 -0600

    giving options for user to select from when typing the input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 function App() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
 
-  const onInputChange = (e) => {
-    console.log(e.target.value);
+  const key = import.meta.env.VITE_REACT_APP_API_KEY;
+
+  const getSearchOptions = (location: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${key}`,
+    )
+      .then((res) => res.json())
+      .then((data) => setOptions(data));
   };
 
-  // https://api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const location = e.target.value;
+    setInput(location);
+
+    getSearchOptions(location);
+  };
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
   return (
     <>
@@ -22,13 +36,23 @@ function App() {
             option from the dropdown
           </p>
 
-          <div className="mt-10 flex md:mt-4">
+          <div className="relative mt-10 flex md:mt-4">
             <input
               type="text"
               value={input}
               className="rounded-s-md border-2 border-zinc-100 px-2 py-1 text-zinc-700"
               onChange={onInputChange}
             />
+
+            <ul className="absolute top-7 m-1 rounded-b-md bg-white text-zinc-700">
+              {options.map((option: { name: string }, index: number) => (
+                <li key={option.name + "-" + index}>
+                  <button className="w-full cursor-pointer px-2 py-1 text-left text-sm hover:bg-zinc-700 hover:text-white">
+                    {option.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
 
             <button className="rounded-e-md border-2 border-zinc-100 px-2 py-1 hover:border-zinc-700 hover:text-zinc-700">
               Search
