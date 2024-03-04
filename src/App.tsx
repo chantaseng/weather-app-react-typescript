7785bee347c7a2cdc94bba53b822bd4824995033
William Chan <chan.william.ts@gmail.com>
2024-03-04 17:01:43 -0600

    creating useForecast hook and splitting all the JS function in the hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,9 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { optionType } from "./types";
 import Search from "./components/Search";
+import useForecast from "./hooks/useForecast";
 
 function App() {
-  const [input, setInput] = useState<string>("");
-  const [options, setOptions] = useState<[]>([]);
-  const [city, setCity] = useState<optionType | null>(null);
-  const [forecast, setForecast] = useState<null>(null);
-
-  const key = import.meta.env.VITE_REACT_APP_API_KEY;
-
-  const getSearchOptions = (location: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${key}`,
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data));
-  };
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const location = e.target.value;
-    setInput(location);
-
-    getSearchOptions(location);
-  };
-
-  const getForecast = (city: optionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${key}
-      `,
-    )
-      .then((res) => res.json())
-      .then((data) => setForecast(data));
-  };
-
-  const onSubmit = () => {
-    if (!city) return;
-
-    getForecast(city);
-  };
-
-  const onOptionSelect = (option: optionType) => {
-    setCity(option);
-  };
-
-  useEffect(() => {
-    if (city) {
-      setInput(city.name);
-      setOptions([]);
-    }
-  }, [city]);
-
-  // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={API key} --- SPECIFIC LOCATION
-
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key} --- TYPING FOR OPTIONS OF LOCATION
+  const { input, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast();
 
   return (
     <>
